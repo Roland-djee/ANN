634d4dd5585915c2a8f328685417a0a14c970dd6
Roland Guichard <r.guichard@ucl.ac.uk>
2016-09-14 13:56:46 +0100

    First commit

---
+++ src/ANN.py
@@ -0,0 +1,10 @@
+#!/usr/bin/py
+""" ANN is a rudimentary example of artificial neural network
+for a comprehensive idea of the general process.
+@author Dr. Roland Guichard <r.guichard@ucl.ac.uk>
+@todo: Document code
+"""
+
+import numpy as np
+import scipy as sc
+
